bd4769eb78afa01424077005f51f46642c9aaee8
Mariam Harutyunyan <marharutyun@gmail.com>
2019-12-16 15:28:00 +0400

    Updates

---
+++ precache-manifest.f70d88f981c3c8a9d5b145cf05701ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5675ca46034566407ea6281691d85960",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "e97a3b04708000655190",
+    "url": "/react-blog/static/css/main.1dcf0ca5.chunk.css"
+  },
+  {
+    "revision": "6a29daa85c971b654f2e",
+    "url": "/react-blog/static/js/2.654f76cc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-blog/static/js/2.654f76cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e97a3b04708000655190",
+    "url": "/react-blog/static/js/main.fa1ec06f.chunk.js"
+  },
+  {
+    "revision": "3864e0010ebc5425bb03",
+    "url": "/react-blog/static/js/runtime-main.d95296d1.js"
+  },
+  {
+    "revision": "a5a534ad5a053bcaba81a1ee8fb25dc6",
+    "url": "/react-blog/static/media/avatar.a5a534ad.png"
+  }
+]);
